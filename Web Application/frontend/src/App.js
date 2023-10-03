9194dbe7305ac8e6b7b4194002e5b346a82eb73c
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-03 13:26:20 +0530

    Created supplier login

--- Web Application/frontend/src/App.js
+++ Web Application/frontend/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Test from './components/Test';
+import Test from "./components/Test";
+import LoginSupplier from "./components/Logins/LoginSupplier";
 
 function App() {
   return (
     <Router>
       <Route path="/test" exact component={Test} />
+      <Route path="/loginsupplier" exact component={LoginSupplier} />
     </Router>
   );
 }
