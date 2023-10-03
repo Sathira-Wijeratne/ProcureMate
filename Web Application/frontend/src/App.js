bcd0e7bfc20d88b28b521634841570eff484af19
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-03 14:56:26 +0530

    Created logins

--- Web Application/frontend/src/App.js
+++ Web Application/frontend/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Test from "./components/Test";
 import LoginSupplier from "./components/Logins/LoginSupplier";
+import LoginStaffMember from "./components/Logins/LoginStaffMember";
 
 function App() {
   return (
     <Router>
       <Route path="/test" exact component={Test} />
+      <Route path="/" exact component={LoginStaffMember} />
       <Route path="/loginsupplier" exact component={LoginSupplier} />
     </Router>
   );
