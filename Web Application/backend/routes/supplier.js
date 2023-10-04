98fdf2ed55d91dba88ee82ba57106260f6d4d6b3
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-04 17:09:27 +0530

    Created getPending order route

--- Web Application/backend/routes/supplier.js
+++ Web Application/backend/routes/supplier.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 let Supplier = require("../models/Supplier");
+let PurchaseOrder = require("../models/PurchaseOrder");
 
 // Get all suppliers
 router.route("/").get((req, res) => {
@@ -24,6 +25,24 @@ router.route("/get/email/:email").get(async (req, res) => {
       res
         .status(500)
         .send({ status: "Error with get the supplier", error: err.message });
+    });
+});
+
+// Get supplier by email
+router.route("/getpendingorders/:supplierid").get(async (req, res) => {
+  let supplierId = req.params.supplierid;
+  await PurchaseOrder.find({ supplierId: supplierId, status: "Approved" })
+    .then((purchaseOrders) => {
+      res.json(purchaseOrders);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({
+          status: "Error with get the purchase orders",
+          error: err.message,
+        });
     });
 });
 
