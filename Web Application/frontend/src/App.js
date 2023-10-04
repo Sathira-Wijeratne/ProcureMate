f353f55b9482fb3ea79001bfb4507260b63fdf4f
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-04 12:49:01 +0530

    Change folder structure

--- Web Application/frontend/src/App.js
+++ Web Application/frontend/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Test from "./components/Test";
-import LoginSupplier from "./components/Logins/LoginSupplier";
-import LoginStaffMember from "./components/Logins/LoginStaffMember";
+import LoginSupplier from "./components/login_components/LoginSupplier";
+import LoginStaffMember from "./components/login_components/LoginStaffMember";
 
 function App() {
   return (
