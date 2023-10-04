2ab93cc263583b9e2994c3cf4da4ab9d3b3a3817
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-04 11:24:55 +0530

    Created delivery note model

---
+++ Web Application/backend/models/DeliveryNote.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// define a new schema for delivery note
+const deliveryNoteSchema = new Schema({
+  deliveryId: {
+    type: String,
+    required: true,
+  },
+  pOrderId: {
+    type: String,
+    required: true,
+  },
+  supplierId: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  itemName: {
+    type: String,
+    required: true,
+  },
+  qty: {
+    type: Number,
+    required: true,
+  },
+  uom: {
+    type: String,
+    required: false,
+  },
+  siteMngId: {
+    type: String,
+    required: true,
+  },
+  siteId: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+});
+
+// create a model based on the delivery note schema
+const DeliveryNote = mongoose.model("DeliveryNote", deliveryNoteSchema);
+
+// export the delivery note model to be used in other parts of the application
+module.exports = DeliveryNote;
