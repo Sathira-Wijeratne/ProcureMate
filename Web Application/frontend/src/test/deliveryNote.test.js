e60e26904e54698264cd256ae2adc22c51556fb3
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-10-08 23:38:05 +0530

    Implemented 2nd test case of the deliverynote

--- Web Application/frontend/src/test/deliveryNote.test.js
+++ Web Application/frontend/src/test/deliveryNote.test.js
@@ -1,8 +1,51 @@
 import axios from "axios";
 
 // Test creating delivery note
-describe("Testing creating delivery note", () => {
+describe("Testing delivery note creation", () => {
   it("Should get a valid status", async () => {
+    const deliveryNote = {
+      deliveryId: "#D-TEST-TEMP",
+      pOrderId: "#P-TEST-TEMP",
+      supplierId: "S-TEST-TEMP",
+      date: new Date(),
+      status: "Sent",
+      itemName: "TEST-ITEM-TEMP",
+      qty: 10,
+      uom: "kg",
+      siteMngId: "E-TEST-TEMP",
+      siteId: "#S-TEST-TEMP",
+      location: "TEST-LOCATION-TEMP",
+    };
+    const res1 = await axios.post(
+      `http://localhost:8070/supplier/createdeliverynote/`,
+      deliveryNote
+    );
+    expect(res1.status).toBe(200);
+
+    const res2 = await axios.get(
+      `http://localhost:8070/supplier/getdeliverynote/${deliveryNote.deliveryId.substring(
+        1
+      )}`
+    );
+    expect(res2.data[0].deliveryId).toBe(deliveryNote.deliveryId);
+    expect(res2.data[0].pOrderId).toBe(deliveryNote.pOrderId);
+    expect(res2.data[0].itemName).toBe(deliveryNote.itemName);
+    expect(res2.data[0].qty).not.toBe(deliveryNote.qty + 1);
+    expect(res2.data[0].uom).not.toBe(deliveryNote.uom + "s");
+    expect(res2.data[0].qty).not.toBe(deliveryNote.siteMngId + "MNG");
+    const res3 = await axios.delete(
+      `http://localhost:8070/supplier/deletedeliverynote/${deliveryNote.deliveryId.substring(
+        1
+      )}`
+    );
+    expect(res3.status).toBe(200);
+  });
+});
+
+// Testing existing delivery note
+describe("Testing existing delivery note", () => {
+  it("Should get a valid status", async () => {
+    // existing delivery note
     const deliveryNote = {
       deliveryId: "#D-TEST",
       pOrderId: "#P-TEST",
@@ -16,22 +59,19 @@ describe("Testing creating delivery note", () => {
       siteId: "#S-TEST",
       location: "TEST-LOCATION",
     };
-    const res1 = await axios.post(
-      `http://localhost:8070/supplier/createdeliverynote/`,
-      deliveryNote
-    );
-    expect(res1.status).toBe(200);
 
-    const res2 = await axios.get(
-      "http://localhost:8070/supplier/getdeliverynote/D-TEST"
+    const res = await axios.get(
+      `http://localhost:8070/supplier/getdeliverynote/${deliveryNote.deliveryId.substring(
+        1
+      )}`
     );
-    expect(res2.data[0].deliveryId).toBe(deliveryNote.deliveryId);
-    expect(res2.data[0].pOrderId).toBe(deliveryNote.pOrderId);
-    expect(res2.data[0].itemName).toBe(deliveryNote.itemName);
-
-    const res3 = await axios.delete(
-      `http://localhost:8070/supplier/deletedeliverynote/D-TEST`
-    );
-    expect(res3.status).toBe(200);
+    expect(res.data[0].deliveryId).toBe(deliveryNote.deliveryId);
+    expect(res.data[0].pOrderId).toBe(deliveryNote.pOrderId);
+    expect(res.data[0].itemName).toBe(deliveryNote.itemName);
+    expect(res.data[0].qty).not.toBe(deliveryNote.qty + 1);
+    expect(res.data[0].uom).not.toBe(deliveryNote.uom + "s");
+    expect(res.data[0].qty).not.toBe(deliveryNote.siteMngId + "MNG");
   });
 });
+
+// Testing updating delivery note
