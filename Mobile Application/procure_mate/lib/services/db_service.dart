e0e3d182f20d384b653f6b773e1cb601789b13d6
SanuthiVihansa <subasofts@gmail.com>
2023-10-11 05:41:58 +0530

    Commit changes in db_service

--- Mobile Application/procure_mate/lib/services/db_service.dart
+++ Mobile Application/procure_mate/lib/services/db_service.dart
@@ -7,11 +7,13 @@ class DBService {
       "mongodb+srv://procureMate-db-admin:59RtgdWlrqMZRHhp@cluster0.iwgh5pm.mongodb.net/procureMate_DB?retryWrites=true&w=majority";
   static var db;
   static var staffMemberCollection;
+  static var deliveryNoteCollection;
 
   static connect() async {
     db = await Db.create(mongoConnUrl);
     await db.open();
     staffMemberCollection = db.collection("staffmembers");
+    deliveryNoteCollection = db.collection("deliverynotes");
   }
 
   static Future<List<Map<String, dynamic>>> login(
@@ -22,6 +24,20 @@ class DBService {
       return users;
     } catch (e) {
       print(e);
+      return Future.value(e as FutureOr<List<Map<String, dynamic>>>?);
+    }
+  }
+
+  //fetch the delivery note
+static Future <List<Map<String, dynamic>>> getAllApprovalPendingDeliveryNotes(String siteMngId
+
+    )
+  async{
+    try{
+      final query = {"siteMngId": siteMngId};
+      final deliveryNotes = await deliveryNoteCollection.find(query).toList();
+      return deliveryNotes;
+    }catch(e){
       return Future.value(e as FutureOr<List<Map<String, dynamic>>>?);
     }
   }
