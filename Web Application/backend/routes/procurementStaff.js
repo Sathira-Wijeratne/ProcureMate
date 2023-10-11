2ab65beb9463edeacce79a80643afeb4efac42e8
Sathira Wijeratne <swije27@gmail.com>
2023-10-12 00:17:05 +0530

    implemented route to get employee name

--- Web Application/backend/routes/procurementStaff.js
+++ Web Application/backend/routes/procurementStaff.js
@@ -17,4 +17,20 @@ router.route("/").get(async (req, res) => {
         });
 });
 
+//get staff member name
+router.route("/get/staff/member/name/:id").get(async (req, res) => {
+    let id = req.params.id;
+    await StaffMember.find({ email: `${id}` }, {name:1, _id:0})
+        .then((staffMember) => {
+            res.json(staffMember);
+        })
+        .catch((err) => {
+            console.log(err.message);
+            res.status(500).send({
+                status: "Error with geting the staff member name",
+                error: err.message
+            });
+        });
+});
+
 module.exports = router;
