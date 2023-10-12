b1628f73986e824713c88151c9db3c25ff709c91
Sathira Wijeratne <swije27@gmail.com>
2023-10-12 12:28:55 +0530

    implemented Raised Orders page tab

--- Web Application/frontend/src/common/ProcurementStaffCommonConstants.js
+++ Web Application/frontend/src/common/ProcurementStaffCommonConstants.js
@@ -1,6 +1,11 @@
 const ProcurementConstants = {
-    RAISED_ORDERS_TITLE : 'Requisition Approval',
-    RAISED_ORDERS_USER_ROLE : 'Procurement Staff'
-};
+    // RAISED ORDERS PAGE
+    RAISED_ORDERS_PAGE_TITLE: 'Requisition Approval',
+    RAISED_ORDERS_USER_ROLE: 'Procurement Staff',
+    RAISED_ORDERS_TAB1_NAME: 'Raised Orders',
+    RAISED_ORDERS_TAB2_NAME: 'Approved Orders',
+    RAISED_ORDERS_TAB3_NAME: 'Rejected Orders',
+    RAISED_ORDERS_TAB4_NAME: 'Direct Placed Orders',
+}
 
 export default ProcurementConstants;
--- Web Application/frontend/src/components/procurement_staff_components/RaisedOrders.js
+++ Web Application/frontend/src/components/procurement_staff_components/RaisedOrders.js
@@ -1,6 +1,7 @@
 import ProcurementConstants from "../../common/ProcurementStaffCommonConstants";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { BsFillStarFill, BsMenuButtonWideFill } from "react-icons/bs";
 
 export default function RaisedOrders() {
     if (sessionStorage.getItem("prMateTnemerucorp") === null) {
@@ -12,11 +13,11 @@ export default function RaisedOrders() {
 
     useEffect(() => {
         axios.get(`http://localhost:8070/procurement/get/staff/member/name/${employeeEmail}`)
-        .then((res) => {
-            console.log(res.data[0].name);
-            setEmployeeName(res.data[0].name);
-        })
-    },[]);
+            .then((res) => {
+                console.log(res.data[0].name);
+                setEmployeeName(res.data[0].name);
+            })
+    }, []);
 
     return (
         <div>
@@ -36,12 +37,58 @@ export default function RaisedOrders() {
                             marginTop: "5%",
                         }}
                     >
-                        <b>{ProcurementConstants.RAISED_ORDERS_TITLE}</b>
+                        <b>{ProcurementConstants.RAISED_ORDERS_PAGE_TITLE}</b>
                     </div>
                     <div id="procurement-raised-orders-profile-details" style={{ textAlign: "center", marginTop: "3%" }}>
                         <b>{employeeName}</b>
                         <br />
                         {ProcurementConstants.RAISED_ORDERS_USER_ROLE}
+                    </div>
+                    <div id="procurement-raised-orders-tabs" style={{ marginTop: "8%", fontSize: "150%", marginLeft: "10%" }}>
+                        <div id="procurement-raised-orders-tabs-1" style={{marginBottom: "30px"}}>
+                            <a style={{ textDecoration: "none" }}>
+                                <BsFillStarFill
+                                    style={{ marginBottom: "2%", marginRight: "5%", color: "#3a7ae0" }}
+                                />
+                            </a>
+                            <b style={{ color: "#3a7ae0" }}>{ProcurementConstants.RAISED_ORDERS_TAB1_NAME}</b>
+                        </div>
+                        <div id="procurement-raised-orders-tabs-2" style={{marginBottom: "30px"}}>
+                            <a style={{ textDecoration: "none" }}>
+                                <BsMenuButtonWideFill
+                                    style={{
+                                        marginBottom: "1%",
+                                        marginRight: "5%",
+                                        color: "black",
+                                    }}
+                                />
+                            </a>
+                            <b>{ProcurementConstants.RAISED_ORDERS_TAB2_NAME}</b>
+                        </div>
+                        <div id="procurement-raised-orders-tabs-2" style={{marginBottom: "30px"}}>
+                            <a style={{ textDecoration: "none" }}>
+                                <BsMenuButtonWideFill
+                                    style={{
+                                        marginBottom: "1%",
+                                        marginRight: "5%",
+                                        color: "black",
+                                    }}
+                                />
+                            </a>
+                            <b>{ProcurementConstants.RAISED_ORDERS_TAB3_NAME}</b>
+                        </div>
+                        <div id="procurement-raised-orders-tabs-2" style={{marginBottom: "30px"}}>
+                            <a style={{ textDecoration: "none" }}>
+                                <BsMenuButtonWideFill
+                                    style={{
+                                        marginBottom: "1%",
+                                        marginRight: "5%",
+                                        color: "black",
+                                    }}
+                                />
+                            </a>
+                            <b>{ProcurementConstants.RAISED_ORDERS_TAB4_NAME}</b>
+                        </div>
                     </div>
                 </div>
             </div>
