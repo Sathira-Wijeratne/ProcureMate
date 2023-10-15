448708fcc4989d83fb96fd81278316d0ba64fc54
Mona Lisa <it21127946@my.sliit.lk>
2023-10-15 16:32:14 +0530

    Routes added to App.js

--- Web Application/frontend/src/App.js
+++ Web Application/frontend/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Test from './components/Test';
-
+import DeliveryDetailsTable from './components/payment-settlement/POandDOdetails';
+import PendingInvoices from './components/payment-settlement/PendingInvoices';
 function App() {
   return (
     <Router>
       <Route path="/test" exact component={Test} />
-    </Router>
+      <Route path="/purchaseOrderPayment" exact component={ DeliveryDetailsTable }/>
+      <Route path="/pendingInvoices" exact component={ PendingInvoices }/>
+          </Router>
   );
 }
 
