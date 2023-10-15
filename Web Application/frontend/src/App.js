bd16769a354d9129aed7bddb2f554c7c844ec0fd
Mona Lisa <it21127946@my.sliit.lk>
2023-10-15 17:03:58 +0530

    Components with routes called.

--- Web Application/frontend/src/App.js
+++ Web Application/frontend/src/App.js
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Test from './components/Test';
 import DeliveryDetailsTable from './components/payment-settlement/POandDOdetails';
 import PendingInvoices from './components/payment-settlement/PendingInvoices';
+import DeliveryLogs from './components/payment-settlement/Deliver_LogsFile';
 function App() {
   return (
     <Router>
       <Route path="/test" exact component={Test} />
       <Route path="/purchaseOrderPayment" exact component={ DeliveryDetailsTable }/>
-      <Route path="/pendingInvoices" exact component={ PendingInvoices }/>
+      <Route path="/accountinghome/pendingInvoices" exact component={ PendingInvoices }/>
+      <Route path="/accountinghome/deliveryLogs" exact component={ DeliveryLogs }/>
+      
           </Router>
   );
 }
